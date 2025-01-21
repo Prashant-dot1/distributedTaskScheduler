2e0c648f23a9ea508f35547983f11dc31cd44f69
Prashant-dot1 <puriprashant14@gmail.com>
2025-01-21 12:23:56 +0530

    trait definitions for the state store

---
+++ scheduler_core/src/state.rs
@@ -0,0 +1,11 @@
+use crate::{error::SchedulerError, task::Task, task::TaskStatus};
+use async_trait::async_trait;
+use uuid::Uuid;
+
+#[async_trait]
+pub trait StateStore {
+    async fn store_task(&self, task : &Task) -> Result<(), SchedulerError>;
+    async fn get_task(&self, task_id: Uuid) -> Result<Option<Task> , SchedulerError>;
+    async fn update_task(&self, task_id: Uuid, status: TaskStatus) -> Result<(), SchedulerError>;
+    async fn get_pending_tasks(&self) -> Result<Vec<Task>, SchedulerError>;
+}
